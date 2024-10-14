495b251db8b5df9263d006d14734576c0aceb1ee
KANDUKURI HARSHITHA <kandukuriharshitha02@gmail.com>
2024-10-14 06:05:20 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,6 @@
+//JavaScript is dynamically typed, meaning types are determined at runtime, and variables 
+// can hold values of any type without prior declaration or type enforcement.
+
 "use strict"; // treat all JS code as newer version 
 
 // alert("hello!!") --> works on the browser but not in the node.js (available but will be used later)
@@ -14,7 +17,8 @@ let name ="Hkay"
 let age=20
 let isLoggesIn=true 
 
-//PRIMITIVE DATATYPES 
+//PRIMITIVE DATATYPES (PASS BY VALUE) : When primitive types are passed to a function, a copy of the value is made. 
+//     Any changes to the parameter inside the function won’t affect the original variable.
 // number --> 2^53 
 // bigint 
 // string --> ""
@@ -23,13 +27,18 @@ let isLoggesIn=true
 // undefined --> still the value is not assigned 
 // symbol --> to find the uniqueness 
 
-//NON-PRIMITIVE DATATYPES 
+//NON-PRIMITIVE DATATYPES (PASS BY REFERENCE): When objects (including arrays) are passed to a function, the reference to the original object is passed. 
+//      If the function modifies the object's properties, those changes will reflect outside the function.
 // Object 
 // Array 
-// Set 
-// Map
+// Function 
 
 //typeof 
 console.log(typeof "Hkay");
 console.log(typeof null);        //--> object 
 console.log(typeof undefined);   //--> undefined 
+/* 
+    undefined - undefined 
+    null - object 
+    
+*/ 
